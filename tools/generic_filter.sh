d61fd0bd6777ceb52535e550e5a381b3996ed615
Ignacio Manuel Lebrero Rial <imlrial@ARARimlrial.local>
2016-06-12 16:42:31 -0300

    al generico

--- tools/generic_filter.sh
+++ tools/generic_filter.sh
@@ -2,21 +2,12 @@
 # Filtrar por input usando awk.
 
 # -------------- Datos ------------ #
-# $0 es la columna a filtrar        #
-# $1 es el dato a tener en cuenta   #
-# $2 año de inicio                  #
-# $3 año de fin                     #
+# $1 es la columna a filtrar        #
+# $2 es el dato a tener en cuenta   #
+# $3 archivo de entrada				#
+# $4 archivo de salida              #
 #####################################
 
-for i in `seq $3 $4`;
-do
-	#"../data/200"$i".csv"
-	infile="../data/"$i".csv"
-
-	# el path es /data/filtrados/<columnafiltrada>_<datofiltrado>_<año>_data.csv
-	outfile="../data/filtrados/"$1"_"$2"_"$i"_data.csv"
-
-	 #Ojo con la secuencia de " y ' al filtrar por &&. 
-	 #Filtro la columna
-	awk -F, '$'"$1"' == "'"$2"'"' $infile > $outfile
-done
+ #Ojo con la secuencia de " y ' al filtrar por &&. 
+ #Filtro la columna
+awk -F, '$'"$1"' == "'"$2"'"' $3 > $4
